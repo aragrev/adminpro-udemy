5a0e7fba5b289abea711c823d71551bdf0788f9a
aragrev <ruben.vergara@live.cl>
2019-02-17 00:15:06 -0300

    Input, Output y Viewchild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 // Modulo Shared
 import { SharedModule } from '../shared/shared.module';
@@ -6,18 +7,27 @@ import { SharedModule } from '../shared/shared.module';
 // Rutas
 import { PAGES_ROUTES } from './pages.routes';
 
+// ng2-charts
+import { ChartsModule } from 'ng2-charts';
+
 // Componentes
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 
+// Temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
 @NgModule({
     declarations: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     exports: [
         PagesComponent,
@@ -26,7 +36,9 @@ import { Graficas1Component } from './graficas1/graficas1.component';
         Graficas1Component
     ], imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
     ]
 })
 export class PagesModule {}
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent implements OnInit {
 
+  progreso1: number = 20;
+  progreso2: number = 40;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  // función para recibir el evento
+  // actualizar(event: number) {
+  //   console.log('Evento: ', event);
+  // }
 
 }
